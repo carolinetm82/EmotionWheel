2d9a468c49316e1df50ffdd285a4e254c9d1836a
carolinetm82 <thuymicao@gmail.com>
2020-12-18 13:39:48 +0100

    files.py modified

--- EmotionDash/callbacks.py
+++ EmotionDash/callbacks.py
@@ -1,23 +1,43 @@
-from dash.dependencies import Input, Output
-from layouts import df1,df2,urllib
+from dash.dependencies import Input, Output, State
+from layouts import df1,df2,urllib,dash_table,joblib,filename1
 from app import app
 from functions import px
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-
-
-'''
+# Creating a callback so we can replace a dataframe table by another table with the dropdown option
 @app.callback(
-    Output('research-vs-world_rank', 'figure'),
-    Input('dropdown-univ', 'value'))
-def update_figure(selected_country):
-    filtered_df = df2[df2.country == selected_country]
+    Output('table-raw', 'children'),
+    Input('demo-dropdown', 'value'))
+def update_table(value):
+    if value == 'df1':
+        df= df1
+    elif value == 'df2':
+        df=df2
+    table=dash_table.DataTable(
+          css=[{'selector': '.row', 'rule': 'margin: 0'}],    
+          data=df.to_dict('records'),
+          columns=[{'id': c, 'name': c} for c in df.columns],
+          fixed_rows={'headers': True},
+          page_size=50,
+          style_data={'whiteSpace': 'normal', 'height': 'auto'},
+          style_header={'backgroundColor': 'rgb(193, 205, 205)'},
+          style_cell={'maxWidth': '400px','backgroundColor': 'rgb(202, 225, 255)','color': 'rgb(0, 0, 139)','fontSize':12, 'font-family':'sans-serif'},
+          style_table={'height': '300px','overflowY': 'auto'},
+          id='table1'
+          )
+    return table
 
-# Create the scatter plot
-    fig1 = px.scatter(filtered_df, x="world_rank", y="research",
-                 hover_name="university_name",
-                 title="Research vs Rank for the top 50 universities (select a country with the scroll bar above)")
+# Creating a callback calling an Emotion predictor
+@app.callback(
+    Output("example-output", "children"), [Input("example-button", "n_clicks")],[State("input", "value")]
+)
+def output_text(n,value):
+    if (value is None) or (value==''):
+        return "Please type a word or text to get the emotion"
+    else:    
+        emotion=joblib.load(filename1).predict([value])
+        return emotion
 
-    fig1.update_layout(transition_duration=500)
 
-    return fig1
-'''
--- EmotionDash/layouts.py
+++ EmotionDash/layouts.py
@@ -1,4 +1,5 @@
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_table
 import urllib
@@ -7,63 +8,50 @@ from joblib import load
 from functions import *
 
 
-# Create an index page for homepage
+# Create an index page for homepage that contains an emotion predictor
 index_page = html.Div([
     html.H1('Welcome to the dashboard of the Wheel of Emotions'),
     dcc.Markdown('''
     In this dashboard we will only see the results of the raw data from Kaggle,
     we only removed the stopwords, there is no other preprocessing.
-    '''),    
+    '''), 
+    html.Br(),
+    html.Br(),
+    dcc.Markdown('''
+    **Emotions Predictor**
+    '''),
+    dbc.Input(id="input", placeholder="Type something...", type="text"),
+    dbc.Button("Submit", id="example-button", className="mr-2"),
+    html.Br(),
+    html.P(id="output"),
+    html.Span(id="example-output", style={"vertical-align": "middle"}),
+
 ])
 
 # Create page 1
 layout1 = html.Div([
-    html.H4(children='The Wheel of Emotions'),
-    # Create the data table containing the universities dataset
-    dcc.Tabs([
-        dcc.Tab(label='Kaggle', children=[
-          dash_table.DataTable(
-          css=[{'selector': '.row', 'rule': 'margin: 0'}],    
-          data=df1.to_dict('records'),
-          columns=[{'id': c, 'name': c} for c in df1.columns],
-          fixed_rows={'headers': True},
-          page_size=30,
-          style_data={'whiteSpace': 'normal', 'height': 'auto'},
-          style_header={'backgroundColor': 'rgb(193, 205, 205)'},
-          style_cell={'maxWidth': '400px','backgroundColor': 'rgb(202, 225, 255)','color': 'rgb(0, 0, 139)','fontSize':12, 'font-family':'sans-serif'},
-          style_table={'height': '300px','overflowY': 'auto'},
-          id='table1'
-          )]
-        ),
-    
-        dcc.Tab(label='data.world', children=[
-          dash_table.DataTable(
-          css=[{'selector': '.row', 'rule': 'margin: 0'}],    
-          data=df2.to_dict('records'),
-          columns=[{'id': c, 'name': c} for c in df2.columns],
-          fixed_rows={'headers': True},
-          page_size=30,
-          style_data={'whiteSpace': 'normal', 'height': 'auto'},
-          style_header={'backgroundColor': 'rgb(193, 205, 205)'},
-          style_cell={'maxWidth': '400px','backgroundColor': 'rgb(202, 225, 255)','color': 'rgb(0, 0, 139)','fontSize':12, 'font-family':'sans-serif'},
-          style_table={'height': '300px','overflowY': 'auto'},
-          id='table2'
-          )] 
-        )
-    ]),
+    html.H4(children='Emotions Data Analysis'),
+    # Create the data tables Kaggle and dataworld dataset 
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[
+            {'label': 'Kaggle', 'value': 'df1'},
+            {'label': 'dataworld', 'value': 'df2'},
+        ],
+        value='df1'
+    ),
+    html.Div(id='table-raw'), 
     html.Br(),
     html.Br(),
+    # Adding histogram of Emotions and histogram of words
     dcc.Graph(
         id='count-emotions',
         figure=fig1
-),
+    ),
     dcc.Graph(
         id='count-words',
         figure=fig2
-),
-
-
-
+    ),
     html.Br(),
     dcc.Link('Go back to home', href='/')
 ])
@@ -91,19 +79,18 @@ with open(filename,'rb') as f:
 # Create page 2
 layout2 = html.Div([
     html.H3('Classifiers Results'),
-    dcc.Tab(label='res', children=[
-          dash_table.DataTable(
-          css=[{'selector': '.row', 'rule': 'margin: 0'}],    
-          data=printTable.to_dict('records'),
-          columns=[{'id': c, 'name': c} for c in printTable.columns],
-          page_size=30,
-          style_data={'whiteSpace': 'normal', 'height': 'auto'},
-          style_header={'backgroundColor': 'rgb(193, 205, 205)'},
-          style_cell={'maxWidth': '400px','backgroundColor': 'rgb(202, 225, 255)','color': 'rgb(0, 0, 139)','fontSize':12, 'font-family':'sans-serif'},
-          style_table={'height': '300px'},
-          id='table3'
-          )] 
-        ),
+    # Display classifier results
+    dash_table.DataTable(
+    css=[{'selector': '.row', 'rule': 'margin: 0'}],    
+    data=printTable.to_dict('records'),
+    columns=[{'id': c, 'name': c} for c in printTable.columns],
+    page_size=30,
+    style_data={'whiteSpace': 'normal', 'height': 'auto'},
+    style_header={'backgroundColor': 'rgb(193, 205, 205)'},
+    style_cell={'maxWidth': '400px','backgroundColor': 'rgb(202, 225, 255)','color': 'rgb(0, 0, 139)','fontSize':12, 'font-family':'sans-serif'},
+    style_table={'height': '200px'},
+    id='table3'
+    ),
     dcc.Markdown('''
     For the figures below we will only use the best classifier we found: the pipeline (Countvectorizer and Logistic Regression)
 
@@ -112,10 +99,18 @@ layout2 = html.Div([
         id='roc-curve',
         figure=fig3
     ),
-    dcc.Graph(
-        id='tpr-fpr-curve',
-        figure=fig4
+    html.Div([
+    dbc.Row(
+        [
+            dbc.Col(html.Div(dcc.Graph(id='tpr-fpr-curve',figure=fig4))),
+            dbc.Col(html.Div(dcc.Markdown('''The area under curve(AUC) summarizes the skill of a 
+            model across thresholds whereas the F1-score summarizes model skill for a specific probability threshold.  
+            For each emotion we have an AUC of 0.99 
+            or 0.98 which means their predictions are 98% or 99% correct 
+            and the model has an excellent skill''')), width=4),
+        ]
     ),
+    ]),
     dcc.Graph(
         id='matrix',
         figure=fig5
